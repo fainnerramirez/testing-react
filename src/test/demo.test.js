a32cb79ddc05a4552b8cf3db6451d724a3735705
fainnerramirez <fainnerramirez@gmail.com>
2021-12-31 13:18:56 -0500

    add demo.test.js

---
+++ src/test/demo.test.js
@@ -0,0 +1,8 @@
+describe("Pruebas en el archivo demo.test.js", () => {
+  test("deben ser iguales los strings", () => {
+    const mensaje = "Hola mundo";
+    const mensaje2 = "Hola mundo";
+
+    expect(mensaje).toBe(mensaje2); // mensaje === mensaje2
+  });
+});
