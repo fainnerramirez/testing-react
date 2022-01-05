a2ac8ca70a92c9e193c4d4fc93593b1e23c4afd7
fainnerramirez <fainnerramirez@gmail.com>
2022-01-05 16:08:39 -0500

    add test simulate events button

--- src/test/CounterApp.test.js
+++ src/test/CounterApp.test.js
@@ -2,21 +2,26 @@ import { shallow } from "enzyme";
 import CounterApp from "../CounterApp";
 
 describe("Pruebas en el componente <CounterApp />", () => {
-  test("se debe mostrar correctamente <CounterApp />", () => {
-    const view = shallow(<CounterApp />);
+  const view = shallow(<CounterApp />);
 
+  test("se debe mostrar correctamente <CounterApp />", () => {
     expect(view).toMatchSnapshot();
   });
 
   test("debe de mostrar el valor por defecto de 100", () => {
     const valor = "100";
-
     const view = shallow(<CounterApp value={100} />);
-
     const textoParrafo = view.find("h1").text().trim();
-
-    console.log(textoParrafo);
-
     expect(textoParrafo).toBe(valor);
+  });
+
+  test("Debe de incrementar el valor de count con el boton de '+'", () => {
+    view.find("button").at(0).simulate("click");
+
+    const textCount = view.find("h1").text().trim();
+
+    console.log("click event", textCount);
+
+    expect(textCount).toBe("101");
   });
 });
