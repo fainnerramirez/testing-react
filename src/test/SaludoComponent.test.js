60b5e81c3478487f8158bcce28f37bcea60517fd
fainnerramirez <fainnerramirez@gmail.com>
2022-01-05 15:14:45 -0500

    add test  snapshots

--- src/test/SaludoComponent.test.js
+++ src/test/SaludoComponent.test.js
@@ -1,14 +1,25 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+//import { render, screen } from "@testing-library/react";
+//import SaludoComponent from "../components/Saludo.component";
+
+import { shallow } from "enzyme";
 import SaludoComponent from "../components/Saludo.component";
 
 describe("pruebas del archivo Saludo.component.js", () => {
-  test('debe de mostrar el mensaje "Hola que tal!"', () => {
+  /*test('debe de mostrar el mensaje "Hola que tal!"', () => {
     const saludo = "Hola que tal!";
 
     render(<SaludoComponent saludo="Hola que tal!" />);
 
     expect(screen.getByText(saludo)).toBeInTheDocument();
+  });*/
+
+  test("debe de mostrar el componente <SaludoComponent /> correctamente", () => {
+    const saludo = "Hola que tal!";
+
+    const view = shallow(<SaludoComponent saludo={saludo} />);
+
+    expect(view).toMatchSnapshot();
   });
 });
